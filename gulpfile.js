b619159b045e9ee6feca2ea0a7e146936d73bb7a
AMCorvi <amcorvi@icloud.com>
2016-09-28 20:26:39 -0400

    Gulpfile redacted "hot" webserver configured. Material Design Implemented in Markup. Automated Sass Compilation (type:"gulp sass"). Created npm develpment environment script for quick start. Type "npm run dev" to get started with live sass compiling and livehost on port: 8080!

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
     pug = require('gulp-pug');
 
 // Declare and set variable to take it easy on the typing. 
@@ -18,23 +20,23 @@ var env,
     jsSources,
     htmlSources,
     sassSources, 
-    sassStyles, 
+    cssStyles, 
     typeScriptSources;
 
 
-env = process.env.NODE_ENV || 'developement'
+env = process.env.NODE_ENV || 'development'
 
-if (env === 'development' ) {
+if (env == 'development' ) {
     outputDir = 'build/dev/';
-    sassStyles = 'expanded';
+    cssStyles = 'expanded';
 } else {
-    outputDir = 'build/prod';
-    sassStyles = 'compressed';
+    outputDir = 'build/prod/';
+    cssStyles = 'compressed';
 };
 
 jsSources = ["build/dev/js/*js"];
 htmlSources = ['component/views/*.html'];
-sassSources = ['css/*.scss'];
+sassSources = ['./sass/*.scss'];
 typeScriptSources = ['components/js/*.js'];
 
 
@@ -45,10 +47,37 @@ typeScriptSources = ['components/js/*.js'];
 //
 
 
-gulp.task('build-html', function(){
-    return gulp.src(jsSources)
-        .pipe(pug({}))
-        .pipe(gulp.dest(outputDir + 'views/'));
+
+ 
+gulp.task('sass', function () {
+  return gulp.src(sassSources)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(cssStyles))
+    .pipe(connect.reload());
 
 });
-    
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('reload', function(){
+   gulp.src(outputDir + 'index.html')
+   .pipe(connect.reload())
+});
+
+
+ 
+gulp.task('watch', function() {
+ 
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./build/*/*.html', ['reload']);
+  gulp.watch('css/*.css', ['sass']);
+});
+
+gulp.task('default', ['connect', 'sass','watch'])
