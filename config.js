5c5fe76c236a69d526b3519c16654d9f68d8890e
AMCorvi <AMCorvi@users.noreply.github.com>
2016-10-29 11:29:19 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports= {
-    twitter: {
-         consumer_key:         'mDrjdrddeyVVvgbzt5M1WuJgi',
-    consumer_secret:      'NQuKAMWbuXAPo1PjHZGF7cuqBKtP7SoOf5n7DUT3ZbmxSDDTwQ',
-    access_token:         '43465312-wq1Lkqh2BZrE20fMXbKM3Lw5TKVVdvSnnsMXRqTTY',
-    access_token_secret:  'rZa5CdRg6OvBZPX3qb0VJ8iZXZQjZkSsJuxHjs4J1niK4'  
-    } 
-}
