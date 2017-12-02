fafb885f1c5c1c38ad55aef0b432c6e0438297da
AMCorvi <am@corvi.com>
2017-12-02 00:01:45 -0500

    - Added: Loader styles, markup and angular control flow adjustments

--- dev/scripts/nichiApp.js
+++ dev/scripts/nichiApp.js
@@ -1,150 +1,137 @@
-var app = angular.module('communion', []);
+var app = angular.module("communion", []);
 
+app.factory("soundcloud", [
+  "$http",
+  function($http) {
+    return $http
+      .get(
+        "http://api.soundcloud.com/users/829120/tracks?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw"
+      )
+      .then(
+        //Succesful request function
+        function(response) {
+          return response.data;
+        },
+        //Handle Error func
+        function(err) {
+          return console.log(err.data);
+        }
+      );
+  }
+]);
 
+app.controller("soundCtrl", [
+  "$scope",
+  "soundcloud",
+  "$http",
+  function($scope, soundcloud, $http) {
+    $scope.changeTrack = function(url, image) {
+      var music = document.getElementById("music");
+      var playerImage = document.getElementById("imgs");
+      $http.get(`${url}?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw`).then(
+        function(response) {
+          $scope.swapTo = response.location;
+          return console.log(response.location);
+        },
+        function(err) {
+          return console.log(err.data);
+        }
+      );
+      music.setAttribute(
+        "src",
+        `${url}?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw`
+      );
+      playerImage.setAttribute("style", `background-image: url('${image}')`);
+    };
+    soundcloud.then(function(data) {
+      $scope.communion = {};
+      $scope.communion.tracks = data;
+      return $scope.communion.tracks;
+    });
+  }
+]);
 
+app.factory("hypelatest", [
+  "$http",
+  function($http) {
+    return $http
+      .get("http://hypem.com/playlist/latest/all/json/1/data.js")
+      .then(
+        //Handle Succesful request
+        function(response) {
+          return response.data;
+        },
+        // handle request error
+        function(err) {
+          return console.log(err.data);
+        }
+      );
+  }
+]);
 
-app.factory('soundcloud', ['$http', function($http) {
+app.factory("hypecommunionfavs", [
+  "$http",
+  function($http) {
+    return $http
+      .get("http://hypem.com/playlist/loved/Redee/json/1/data.js")
+      .then(
+        //Handle Succesful request
+        function(response) {
+          return response.data;
+        },
+        // handle request error
+        function(err) {
+          return console.log(err.data);
+        }
+      );
+  }
+]);
 
-    return $http.get('http://api.soundcloud.com/users/829120/tracks?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw')  
-       .then(
-           //Succesful request function
-           function(response){
-               return response.data
-           },
-           //Handle Error func
-           function(err) {
-              return console.log(err.data)
-           }
-       )
-      
-}])
+app.factory("hypepop", [
+  "$http",
+  function($http) {
+    return $http
+      .get("http://hypem.com/playlist/popular/all/json/1/data.js")
+      .then(
+        //Handle Succesful request
+        function(response) {
+          return response.data;
+        },
+        // handle request error
+        function(err) {
+          return console.log(err.data);
+        }
+      );
+  }
+]);
 
+app.controller("hypeCtrl", [
+  "$scope",
+  "hypepop",
+  "hypelatest",
+  "hypecommunionfavs",
+  function($scope, hypepop, hypelatest, hypecommunionfavs) {
 
-app.controller('soundCtrl',['$scope', 'soundcloud', '$http', function($scope, soundcloud, $http) {
-    
-    $scope.changeTrack = function(url, image){
-        var music = document.getElementById('music')
-        var playerImage = document.getElementById('imgs')
-        $http.get(`${url}?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw`)
-            .then(function(response){
-                    $scope.swapTo = response.location
-                    return console.log(response.location)
-                },
-                function(err){
-                    return console.log(err.data)
-                })
-            music.setAttribute('src', `${url}?client_id=6GTzygpXw9qubWUmlmh70ZUHLidrgGJw`)
-            playerImage.setAttribute('style', `background-image: url('${image}')`)
-         }      
-        soundcloud.then(function(data) {
-                $scope.communion = {}
-                $scope.communion.tracks = data
-                return $scope.communion.tracks
-            }
-    )
+    hypelatest.then(function(data) {
+      $scope.hypelatest = data;
+      delete $scope.hypelatest.version;
+      return $scope.hypelatest;
+    });
 
+    hypecommunionfavs.then(function(data) {
+      $scope.hypecommunionfavs = data;
+      delete $scope.hypecommunionfavs.version;
 
-}])
+      return $scope.hypecommunionfavs;
+    });
 
-
-app.factory('hypelatest', ['$http', function($http){
-
-
-
-   return $http.get('http://hypem.com/playlist/latest/all/json/1/data.js')
-        .then(
-            //Handle Succesful request
-            function(response){
-                return response.data
-            },
-
-            // handle request error
-            function(err){
-                return console.log(err.data)
-            }
-
-        )
-}])
-
-
-app.factory('hypecommunionfavs', ['$http', function($http){
-
-
-
-
-    return $http.get('http://hypem.com/playlist/loved/Redee/json/1/data.js')
-        .then(
-            //Handle Succesful request
-            function(response){
-                return response.data
-            },
-
-            // handle request error
-            function(err){
-                return console.log(err.data)
-            }
-
-        )
-
-
-}])
-
-
-
-app.factory('hypepop', ['$http', function($http){
-
-
-   return $http.get("http://hypem.com/playlist/popular/all/json/1/data.js")
-        .then(
-            //Handle Succesful request
-            function(response){
-                return response.data
-            },
-
-            // handle request error
-            function(err){
-                return console.log(err.data)
-            }
-
-        )
-
-
-}])
-
-
-
-
-app.controller('hypeCtrl', ['$scope','hypepop', 'hypelatest', 'hypecommunionfavs', function($scope, hypepop, hypelatest, hypecommunionfavs){
-
-    hypelatest.then(function(data){
-        $scope.hypelatest = data;
-        delete $scope.hypelatest.version
-        return $scope.hypelatest
-    })
-
-    hypecommunionfavs.then(function(data){
-        $scope.hypecommunionfavs = data;
-        delete $scope.hypecommunionfavs.version
-
-        return $scope.hypecommunionfavs
-    })
-
-    hypepop.then(function(data){
-        $scope.hypepop = data;
-        delete $scope.hypepop.version
-        return $scope.hypepop
-    })
-
-}])
-
-
-
-
-
-
-
-
-
+    hypepop.then(function(data) {
+      $scope.hypepop = data;
+      delete $scope.hypepop.version;
+      return $scope.hypepop;
+    });
+  }
+]);
 
 // app.directive('appInfo', function() {
 //   return {
@@ -157,14 +144,6 @@ app.controller('hypeCtrl', ['$scope','hypepop', 'hypelatest', 'hypecommunionfavs
 // });
 //
 
-
-
-
-
-
-
-
-
 /* app.controller('soundCtrl',[function(){
 
     // initiate auth popup
@@ -175,7 +154,6 @@ app.controller('hypeCtrl', ['$scope','hypepop', 'hypelatest', 'hypecommunionfavs
     });
 
 }]) */
-
 
 //soundcloud controller
 /* app.controller('soundCtrl', ['$scope', '$window', '$http', '$resource', '$timeout', function($scope, $window, $http, $resource, $timeout) {
